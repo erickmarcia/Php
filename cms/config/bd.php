225f844a5124ee6988ea18994103592aa17da403
Erick Marcia <emarcia@ine.gob.ni>
2018-12-18 15:37:32 -0600

    Creando mi primer cms

---
+++ cms/config/bd.php
@@ -0,0 +1,11 @@
+<?php
+ return array{
+     'driver' =>'mysql',
+     'host' =>'localhost',
+     'user' =>'homestead',
+     'password' =>'secret',
+     'database' =>'cms',
+     'charset'=>'utf8'
+ }
+
+?>
