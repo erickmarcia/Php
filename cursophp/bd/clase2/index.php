6d7dee7d8c779daace05d0662be71c91ead27774
Erick Marcia <emarcia@ine.gob.ni>
2018-12-19 14:46:30 -0600

    update

--- cursophp/bd/clase2/index.php
+++ cursophp/bd/clase2/index.php
@@ -4,13 +4,11 @@
 	<title>Inicio</title>
 </head>
 <body>
-
 	<h1>Panel de administración</h1>
-
 	<a href="Estudiantes_vista.php">Estudiantes</a>
 	<a href="#">Profesores</a>
 	<a href="#">Cursos</a>
-
-
+	<?php
+?>
 </body>
 </html>
